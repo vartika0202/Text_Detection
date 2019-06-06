54424f4955538968638dd35589e0cf5753d0512f
vartikatyagi02 <vartikadhruv@gmail.com>
2019-06-06 13:24:28 +0530

    first commit

---
+++ source code/main.py
@@ -0,0 +1,36 @@
+import cv2
+import numpy as np
+import pytesseract
+from PIL import Image
+
+# Path
+src_path = "/Users/vartika/Downloads/text/"
+
+def get_string(img_path):
+    # Read image with opencv
+    img = cv2.imread(img_path)
+
+    # Convert to gray
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # remove noise
+    kernel = np.ones((1, 1), np.uint8)
+    img = cv2.dilate(img, kernel, iterations=1)
+    img = cv2.erode(img, kernel, iterations=1)
+
+    # Write image after removed noise
+    cv2.imwrite(src_path + "removed_noise.png", img)
+
+    # Write the image after apply opencv to do some ...
+    cv2.imwrite(src_path + "thres.png", img)
+
+    # Recognize text with tesseract for python
+    result = pytesseract.image_to_string(Image.open(src_path + "thres.png"))
+
+    return result
+
+
+print ('--- RECOGNIZING---')
+print (get_string(src_path + "8.png"))
+
+print ("------ FINISH -------")
